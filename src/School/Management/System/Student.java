88311efcb88fa4068d017a979fdb017b1718673c
Sushant-cap <sushantkamble9284@gmail.com>
2023-02-07 15:51:56 +0530

    added Student class

--- src/School/Management/System/Student.java
+++ src/School/Management/System/Student.java
@@ -1,4 +1,22 @@
 package School.Management.System;
 
+
+/**
+ *
+ * Created by K.Sushant on 07/02/2023
+ *This class is responsible for keeping  the
+ *track the students fees, names, id name ,grade and fees paid
+ *
+ *
+ *
+ */
 public class Student {
+
+    private int id;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+    private String name ;
+
+
 }
